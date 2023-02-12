b20b37e92268a1b8c5a2c4dfd7c19d0ccd602f07
Brandonk1023 <brandonk1023@gmail.com>
2023-02-12 17:24:58 -0500

    corrected parseInt, corrected window alerts

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,7 @@
 //the password is either displayed in alert/printed to page
 //window.prompt()
 //window.alert()
-var passwordLength = 8;
+var passwordLength = 0;
 var passwordCharacterOptions = [];
 var availableCharacters = [];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -15,51 +15,42 @@ var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+", "?", "~", "{", "}", "[", "]", "<", ">",];
 
-var generatePassword = function () {
+function generatePassword() {
 
-  var lengthPrompt = function () {
-    var lengthChoice = parsInt(window.prompt("Enter password length. Can only have 8-128 characters:"));
-    if (!lengthChoice) {
-      return;
-    }
-    else if (lengthChoice === NaN, lengthChoice < 8, lengthChoice > 128) {
-      window.alert("Password length must be specified in digits from 8-128.  Please re-enter desired  password length.");
-      return false;
-    }
-    else {
-      PasswordLength = lengthChoice
-    }
-  };
+  passwordLength = parseInt(window.prompt("Enter password length. Can only have 8-128 characters:"));
+
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    window.alert("Password length must be specified in digits from 8-128.  Please re-enter desired  password length.");
+    return;
+  }
 
   window.alert("You chose " + passwordLength + " as the password length");
 
-  confirm("Should your password have uppercase letters?") {
-    availableCharacters = availableCharacters.concat(upperCase);
-    passwordCharacterOptions.push("uppercase, ");
+  function criteria() {
+    if (confirm("Should your password have uppercase letters?")) {
+      availableCharacters = availableCharacters.concat(upperCase);
+      passwordCharacterOptions.push("uppercase");
+    };
+
+    if (confirm("Should your password have lowercase letters?")) {
+      availableCharacters = availableCharacters.concat(lowerCase);
+      passwordCharacterOptions.push(" lowercase");
+    };
+
+    if (confirm("should your password contain numbers?")) {
+      availableCharacters = availableCharacters.concat(numeric);
+      passwordCharacterOptions.push(" numeric")
+    };
+
+    if (confirm("should your password contain special charracters?")) {
+      availableCharacters = availableCharacters.concat(specialCharacter);
+      passwordCharacterOptions.push(" special characters")
+    } return true;
   };
 
-  confirm("Should your password have lowercase letters?") {
-    availableCharacters = availableCharacters.concat(lowerCase);
-    passwordCharacterOptions.push("lowercase, ");
-  };
-
-  confirm("should your password contain numbers?") {
-    availableCharacters = availableCharacters.concat(numeric);
-    passwordCharacterOptions.push("numeric, ")
-  };
-
-  confirm("should your password contain special charracters?") {
-    availableCharacters = availableCharacters.concat(specialCharacter);
-    passwordCharacterOptions.push("special characters ")
-  };
-
-  var criteriaAlert = function () {
-    var characterChoice = window.alert(" You have chosen " + availableCharacters + "as your password criteria");
-  };
-
-}
-
-window.alert("You have chosen " + passwordCharacterOptions + " for the password criteria");
+  criteria();
+  window.alert(" You have chosen " + passwordCharacterOptions + ", as your password criteria.");
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
